cd2c1dd3833f4cc37041603396c09828f8c8e582
Wizzazi <155719588+WizzyWodich@users.noreply.github.com>
2024-08-18 12:31:14 +0300

    1.4.0 - New methods
    Новые методы обновления

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,12 +1,27 @@
-import disnake
-from disnake import File
+import aiohttp, io, disnake, random
 from disnake.ext import commands
-from easy_pil import Editor, load_image_async, Font
+from PIL import Image, ImageFont, ImageDraw
+from disnake.ext import commands
+from disnake import Permissions
 from database.Welcome_Channel import WelcomeChannel
 
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.welcome_db = WelcomeChannel()
+        
+    async def get_avatar(self, url: str) -> bytes: # алгоритм жля получения и чтения аватара юзера
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                avatar = await resp.read()
+        return avatar
+
+    async def limit_string_length(self, string, max_length=15) -> None: # сокращение никнейма
+        if len(string) > max_length:
+            return f'{string[:max_length-1]}...'
+        else:
+            return string
+        
         
     @commands.Cog.listener()
     async def on_member_join(self, member: disnake.Member):
@@ -14,51 +29,75 @@ class Welcome(commands.Cog):
         welcome_db = WelcomeChannel()
         channel = await welcome_db.get_welcome_channel(guild)
         
-        if isinstance(channel, disnake.abc.Messageable): 
-            if guild.id == 1252177187289698344:  
-                background = Editor("image/welcome_baner/XicbxiDInKw.jpg")
-                
-                if member.avatar:
-                    profile_image = await load_image_async(str(member.avatar.url))
-                else:
-                    profile_image = await load_image_async(str(member.default_avatar.url))
-                
-                profile = Editor(profile_image).resize((250, 250)).circle_image()
-                popins = Font.montserrat(size=100, variant="bold")
-                popins_small = Font.montserrat(size=70, variant="bold")
-                
-                background.paste(profile, (725, 175))
-                background.ellipse((725, 175), 250, 250, outline="white", stroke_width=5)
-                
-                background.text((850, 480), f"WELCOME  {member.name}", color="gold", font=popins, align="center")
-                background.text((810, 580), f"{member.guild.name}", color="gold", font=popins_small, align="center")
-                
-                file = File(fp=background.image_bytes, filename="pic.jpg")
-                await channel.send(file=file)
-                    
-            else:
-                background = Editor("image/welcome_baner/pic.jpg")
-                
-                if member.avatar:
-                    profile_image = await load_image_async(str(member.avatar.url))
-                else:
-                    profile_image = await load_image_async(str(member.default_avatar.url))
-                    
-                    profile = Editor(profile_image).resize((250, 250)).circle_image()
-                    popins = Font.caveat(size=70, variant="bold")
-                    popins_small = Font.caveat(size=50, variant="bold")
-                    
-                    background.paste(profile, (525, 175))
-                    background.ellipse((525, 175), 250, 250, outline="white", stroke_width=5)
-                    
-                    background.text((650, 450), f"WELCOME  {member.name}", color=0x6f7575, font=popins, align="center")
-                    background.text((600, 520), f"{member.guild.name}", color=0x6f7575, font=popins_small, align="center")
-                    
-                    file = File(fp=background.image_bytes, filename="pic.jpg")
-                    await channel.send(file=file)    
+        welcome_pictures_rand = {
+            "pic1": "image/welcome_baner/Pic1.png",
+            "pic2": "image/welcome_baner/Pic2.png",
+            "pic3": "image/welcome_baner/Pic3.png",
+            "pic4": "image/welcome_baner/Pic4.png",
+            "pic5": "image/welcome_baner/Pic5.png",
+            "pic6": "image/welcome_baner/Pic6.png",
+            "pic7": "image/welcome_baner/Pic7.png",
+            "pic8": "image/welcome_baner/Pic8.png",
+            "pic9": "image/welcome_baner/Pic9.png",
+            "pic10": "image/welcome_baner/Pic10.png",
+            "pic11": "image/welcome_baner/Pic11.png",
+            "pic12": "image/welcome_baner/Pic12.png",
+            "pic13": "image/welcome_baner/Pic13.png",
+            "pic14": "image/welcome_baner/Pic14.png",
+            "pic15": "image/welcome_baner/Pic15.png",
+            "pic16": "image/welcome_baner/Pic16.png",
+            "pic17": "image/welcome_baner/Pic17.png",
+            "pic18": "image/welcome_baner/Pic18.png",
+            "pic19": "image/welcome_baner/Pic19.png"
+        }
+        
+        random_background = random.choice(list(welcome_pictures_rand.values()))
+        background = Image.open(random_background)
+        
+        fill_color = "#1A1A1A"
+        
+        backdraw = ImageDraw.Draw(background)
+        font = ImageFont.truetype(font ='font/IBMPlexSerif-LightItalic.ttf', size = 100)
+        
+        # Аватарка
+        avatar = await self.get_avatar(member.display_avatar.url)
+        avatar_image = Image.open(io.BytesIO(avatar))
+        avatar_image = avatar_image.resize((390, 390), Image.LANCZOS)
+        bigsize = (avatar_image.size[0] * 3, avatar_image.size[1] * 3)
+        mask = Image.new("L", bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, 255)
+        mask = mask.resize(avatar_image.size, Image.LANCZOS)
+        background.paste(avatar_image, (764, 84), mask)
+        
+        
+        name = await self.limit_string_length(member.display_name)
+        text_width = backdraw.textbbox((0, 0), name, font=font)[2] - backdraw.textbbox((0, 0), name, font=font)[0]
+        x = (661 - text_width) // 2 + 629
+        backdraw.text((x, 650), name, font=font, fill=fill_color)
+        
+        
+        img_bytes = io.BytesIO()
+        background.save(img_bytes, 'PNG')
+        img_bytes.seek(0)
+        image_file = disnake.File(img_bytes, filename=f'{member.display_name}_profile.png')
+        
+        if channel:
+            await channel.send(file=image_file)    
         else:
-            print(f"Channel '{channel}' is not messageable.")
-
+            pass
+        
+    @commands.slash_command(name="simulate_join", dm_permission=False)
+    @commands.is_owner()
+    async def simulate_join(self, interaction: disnake.ApplicationCommandInteraction):
+        # Получаем участника по ID
+        member = interaction.author
+        if member:
+            await self.on_member_join(member)
+            await interaction.response.send_message("Симуляция присоединения участника выполнена.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Участник не найден.", ephemeral=True)
+        
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
